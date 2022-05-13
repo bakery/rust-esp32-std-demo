c176b57a24a8cd2ac38ef954a23b48919684b923
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2022-05-13 20:21:43 +0100

    Update entrypoint.sh

--- .github/actions/esp-build/entrypoint.sh
+++ .github/actions/esp-build/entrypoint.sh
@@ -1,9 +1,14 @@
-#!/bin/sh -l
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
 
 export PATH=$PATH:/home/esp/.cargo/bin
 . /home/esp/export-rust.sh
 export IDF_TOOLS_PATH=/home/esp/.espressif
 . /home/esp/.espressif/frameworks/esp-idf-v4.4/export.sh
 
+bash -c "set -e;  set -o pipefail; $1"
+
 # export RUST_ESP32_STD_DEMO_WIFI_SSID=ssid; export RUST_ESP32_STD_DEMO_WIFI_PASS=pass; cargo +esp build
-export RUST_ESP32_STD_DEMO_WIFI_SSID=ssid; export RUST_ESP32_STD_DEMO_WIFI_PASS=pass; cargo +esp espflash --partition-table ./partitions.csv save-image firmware-22.bin
+# export RUST_ESP32_STD_DEMO_WIFI_SSID=ssid; export RUST_ESP32_STD_DEMO_WIFI_PASS=pass; cargo +esp espflash --partition-table ./partitions.csv save-image firmware-22.bin
