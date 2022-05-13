3a4510ed170965e24e91e2760674355f2b9c303e
Sergio Gasquez Arcos <sergio.gasquez@gmail.com>
2022-05-13 17:19:17 +0200

    Feature/ota updates (#2)
    * Set user
    
    * Set user
    
    * Change user
    
    * Fix typo
    
    * Test
    
    * Test
    
    * Change direcotry
    
    * Test CI
    
    * Test CI
    
    * Test CI
    
    * Test CI
    
    * Test CI
    
    * Test CI
    
    * Test CI
    
    * Activate esp environment
    
    * Update CI action
    
    * Update sourcing
    
    * Update esp-idf export path
    
    * Formating and cleanup

--- .github/actions/esp-build/entrypoint.sh
+++ .github/actions/esp-build/entrypoint.sh
@@ -1,26 +1,9 @@
 #!/bin/sh -l
 
-echo "hello $1"
-id -u -n
-
 export PATH=$PATH:/home/esp/.cargo/bin
-
-echo "$PATH"
-
-ls -al /home/esp/.cargo/bin
-
-# cargo --help
-
-#ls /home/esp -al
-#chown -R root:root /home/esp
-#ls /home/esp -al
-# /home/esp/export-rust.sh
-# env
-# /home/esp/.cargo/bin/rustup toolchain list
-# rustup toolchain list
-
-# cargo install cargo-espflash
+. /home/esp/export-rust.sh
+export IDF_TOOLS_PATH=/home/esp/.espressif
+. /home/esp/.espressif/frameworks/esp-idf-v4.4/export.sh
 
 export RUST_ESP32_STD_DEMO_WIFI_SSID=ssid; export RUST_ESP32_STD_DEMO_WIFI_PASS=pass; cargo +esp build
-
 # export RUST_ESP32_STD_DEMO_WIFI_SSID=ssid; export RUST_ESP32_STD_DEMO_WIFI_PASS=pass; cargo +esp espflash --partition-table ./partitions.csv save-image firmware-22.bin
